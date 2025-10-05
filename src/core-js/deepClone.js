895939764197a3ad50324184905e1293100d9da2
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-10-05 15:21:14 +0530

    Support circular ref for deepClone.js

--- src/core-js/deepClone.js
+++ src/core-js/deepClone.js
@@ -1,17 +1,26 @@
-export default function deepClone(obj) {
+export default function deepClone(obj, map = new WeakMap()) {
     if (obj === null || typeof obj !== "object") return obj;
 
+    if (map.has(obj)) return map.get(obj)
+
     if (obj instanceof Date) return new Date(obj.getTime());
+    
     if (obj instanceof RegExp) return new RegExp(obj.source, obj.flags);
 
     if (Array.isArray(obj)) {
-        return obj.map(el => deepClone(el))
+        let newArr = []
+        map.set(obj, newArr)
+        obj.forEach((el, i) => {
+            newArr[i] = deepClone(el, map)
+        })
+        return newArr
     }
 
     if (obj instanceof Object) {
-        const newObj = {};
+        const newObj = {}
+        map.set(obj, newObj)
         for (const [key, value] of Object.entries(obj)) {
-            newObj[key] = deepClone(value);
+            newObj[key] = deepClone(value, map)
         }
         return newObj;
     }
